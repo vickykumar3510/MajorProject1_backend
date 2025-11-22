aa6f614353ee382accd2ba39c9b6438058edb047
vickykumar3510 <vicky.kumar3510@gmail.com>
2025-11-22 23:42:05 +0530

    chore: vercel file updated

--- index.js
+++ index.js
@@ -127,6 +127,10 @@ app.get("/books", async(req, res) => {
 
 
 
- module.exports = app;
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log(`Server is running on the PORT ${PORT}`)
+})
+
 
 
